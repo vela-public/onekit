38d2f1d47b6d93e7da329f7e125571585e694c22
edunx <jianguo@eastmoney.com>
2024-09-19 14:37:19 +0800

    update tern

---
+++ ternary/ternary.go
@@ -0,0 +1,15 @@
+package ternary
+
+func T[T any](condition bool, a, b T) T {
+	if condition {
+		return a
+	}
+	return b
+}
+
+func F[T any](fn func() error, a, b T) T {
+	if err := fn(); err != nil {
+		return b
+	}
+	return a
+}
