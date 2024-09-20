6ecca2ebda0a7cd74f32645654ea6fac2ea0e969
edunx <jianguo@eastmoney.com>
2024-09-20 15:46:31 +0800

    netkit update

--- netkit/check.go
+++ netkit/check.go
@@ -1,6 +1,9 @@
 package netkit
 
-import "net"
+import (
+	"net"
+	"strconv"
+)
 
 func Ipv4(addr string) bool {
 	ip := net.ParseIP(addr)
@@ -27,6 +30,28 @@ func Ipv6(addr string) bool {
 		if addr[i] == ':' {
 			return true
 		}
+	}
+
+	return false
+}
+
+func IPPort(v string) bool {
+	host, port, err := net.SplitHostPort(v)
+	if err != nil {
+		return false
+	}
+
+	if !Ipv4(host) && !Ipv6(host) {
+		return false
+	}
+
+	p, err := strconv.Atoi(port)
+	if err != nil {
+		return false
+	}
+
+	if p > 0 && p < 65535 {
+		return true
 	}
 
 	return false
