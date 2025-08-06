93ae9cd3b918d7ff4edd4290488e2b52860a2743
edunx <jianguo@eastmoney.com>
2025-08-06 19:09:13 +0800

    update cond string fsm

--- cond/string.go
+++ cond/string.go
@@ -2,69 +2,100 @@ package cond
 
 import (
 	"github.com/vela-public/onekit/cast"
+	"github.com/vela-public/onekit/jsonkit"
 	"github.com/vela-public/onekit/netkit"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-func String(raw string) Lookup {
-	size := len(raw)
+type StringFSM struct {
+	data string
+	json *jsonkit.FastJSON
+}
 
-	return func(key string) string { // * , ext , ipv4, ipv6 , [1,3]
-		switch key {
-		case "*":
-			return raw
-		case "ext":
-			return filepath.Ext(raw)
-		case "ipv4":
-			return cast.ToString(netkit.Ipv4(raw))
-		case "ipv6":
-			return cast.ToString(netkit.Ipv6(raw))
-		case "ip":
-			return cast.ToString(netkit.Ipv4(raw) || netkit.Ipv6(raw))
-		}
-
-		n := len(key)
-		if n < 3 {
-			return raw
-		}
-
-		if key[0] != '[' {
-			return raw
-		}
-
-		if key[n-1] != ']' {
-			return raw
-		}
-
-		idx := strings.Index(key, ":")
-		if idx < 0 {
-			offset, err := cast.ToIntE(key[1 : n-1])
-			if err != nil {
-				return raw
-			}
-
-			if offset >= 1 && offset <= len(raw) {
-				return string(raw[offset-1])
-			}
-
-			return raw
-		}
-
-		s := cast.ToInt(key[1:idx])
-		e := cast.ToInt(key[idx+1 : n-1])
-		if s > size {
-			return ""
-		}
-
-		if e == 0 || e > size {
-			return raw[s:]
-		}
-
-		if s > e {
-			return ""
-		}
-
-		return raw[s:e]
+func (fsm *StringFSM) UnwrapJson() *jsonkit.FastJSON {
+	if fsm.json == nil {
+		obj := &jsonkit.FastJSON{}
+		obj.ParseText(fsm.data)
+		fsm.json = obj
 	}
+	return fsm.json
+}
+
+func (fsm *StringFSM) Getter(key string) string {
+	sz := len(fsm.data)
+	switch key {
+	case "@len":
+		return strconv.Itoa(len(fsm.data))
+	case "@text":
+		return fsm.data
+	case "ext":
+		return filepath.Ext(fsm.data)
+	case "ipv4":
+		return cast.ToString(netkit.Ipv4(fsm.data))
+	case "ipv6":
+		return cast.ToString(netkit.Ipv6(fsm.data))
+	case "ip":
+		return cast.ToString(netkit.Ipv4(fsm.data) || netkit.Ipv6(fsm.data))
+	}
+
+	if strings.HasPrefix(key, "json:") {
+		k := strings.TrimPrefix(key, "json:")
+		j := fsm.UnwrapJson()
+		if j == nil {
+			return ""
+		}
+		return j.Get(k).String()
+	}
+
+	if key[0] != '[' {
+		return fsm.data
+	}
+
+	if key[sz-1] != ']' {
+		return fsm.data
+	}
+
+	n := len(key)
+	if n < 3 {
+		return fsm.data
+	}
+
+	idx := strings.Index(key, ":")
+	if idx < 0 {
+		offset, err := cast.ToIntE(key[1 : n-1])
+		if err != nil {
+			return fsm.data
+		}
+
+		if offset >= 1 && offset <= sz {
+			return string(fsm.data[offset-1])
+		}
+
+		return fsm.data
+	}
+
+	s := cast.ToInt(key[1:idx])
+	e := cast.ToInt(key[idx+1 : n-1])
+	if s > sz {
+		return ""
+	}
+
+	if e == 0 || e > sz {
+		return fsm.data[s:]
+	}
+
+	if s > e {
+		return ""
+	}
+	return fsm.data[s:e]
+}
+
+func String(raw string) Lookup {
+	fsm := &StringFSM{
+		data: raw,
+	}
+
+	return fsm.Getter
 }
